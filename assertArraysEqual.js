3d155362c0778e7491c127ae73f004b4f4df6f32
srimantika <srimantika@gmail.com>
2021-10-26 21:09:52 +0000

    changed function name

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,6 +1,6 @@
 
 
-const eqArrays = function(Array1, Array2){
+const assertArraysEqual = function(Array1, Array2){
   if (JSON.stringify(Array1) === JSON.stringify(Array2)) {
     console.log(`✅✅✅ Assertion Passed:  ${Array1}  ' === '  ${Array2}`);
    }
@@ -9,5 +9,5 @@ const eqArrays = function(Array1, Array2){
    }
 }
 
-eqArrays([1, 2, 3], [1, 2, 3]);
-eqArrays([1, 2, 3], [3, 2, 3]);
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 3]);
