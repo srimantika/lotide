5cee480c4bbc63a3931748bba4ff9ad8bc7f061b
srimantika <srimantika@gmail.com>
2021-10-27 21:03:50 +0000

    Find letter and its position assignment

---
+++ letterPositions.js
@@ -0,0 +1,23 @@
+const assertArraysEqual = function(Array1, Array2){
+  if (JSON.stringify(Array1) === JSON.stringify(Array2)) {
+    console.log(`✅✅✅ Assertion Passed:  ${Array1}  ' === '  ${Array2}`);
+   }
+   else {
+    console.log(`🛑🛑🛑 Assertion Failed:  ${Array1}  ' !== '  ${Array2}`);
+   }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++){
+    if (results[sentence[i]]) {
+      results[sentence[i]].push(i)
+      } else {
+      results[sentence[i]] = [i];
+      }
+  }
+  return results;
+};
+
+console.log(letterPositions("hello"));
+assertArraysEqual(letterPositions("hello").l, [2,3]);
