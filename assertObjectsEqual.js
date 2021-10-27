0bb6b3cb71cf3e757f4b4650a081293e28d156d5
srimantika <srimantika@gmail.com>
2021-10-27 23:12:11 +0000

    AssertObjectEqual function

---
+++ assertObjectsEqual.js
@@ -0,0 +1,43 @@
+const eqArrays = function(Array1, Array2) {
+  if (JSON.stringify(Array1) === JSON.stringify(Array2)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  } else {
+    for (let i in object1) {
+      if ((Array.isArray(object1[i]) && Array.isArray(object2[i]))) {
+        if (eqArrays(object1[i], object2[i]) === false) {
+          return false;
+        }
+      } else {
+        if (object1[i] !== object2[i]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect; 
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+const abc = { a: "1", b: "2", c: "3" };
+assertObjectsEqual(ab,ba);
+assertObjectsEqual(ab,abc);
