d2864c45d29794041f68b64f2894009b43f5beec
srimantika <srimantika@gmail.com>
2021-10-27 18:57:18 +0000

    count letters in a string

---
+++ countLetters.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: , ${actual} , ' === ' , ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: , ${actual} , ' !== ' , ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+  const result = {};
+  for (let letter of string) {
+    if (letter !== " ") { //filter out the spaces in string
+      if (result[letter]) { ////Increment the count of each letter into results as we encounter each letter in the string.
+        result[letter] += 1;
+      } else {
+        result[letter] = 1;
+      }
+    }
+  }
+  return result;
+};
+
+console.log(countLetters("lighthouse in the house"));
