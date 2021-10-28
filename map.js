94b14a5d3b98b585d413bd534ede58e8a1875b2a
srimantika <srimantika@gmail.com>
2021-10-28 21:30:40 +0000

    creating our own map function

---
+++ map.js
@@ -0,0 +1,22 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const assertArraysEqual = function(Array1, Array2){
+  if (JSON.stringify(Array1) === JSON.stringify(Array2)) {
+    console.log(`✅✅✅ Assertion Passed:  ${Array1}  ' === '  ${Array2}`);
+   }
+   else {
+    console.log(`🛑🛑🛑 Assertion Failed:  ${Array1}  ' !== '  ${Array2}`);
+   }
+};
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const results1 = map(words, word => word[0]);
+console.log(assertArraysEqual(results1, [ 'g', 'c', 't', 'm', 't' ]));
+
