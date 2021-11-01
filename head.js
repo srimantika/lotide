1d679a8ad2bca5ff1d47682f5d4a775a511f1981
srimantika <srimantika@gmail.com>
2021-11-01 17:21:57 +0000

    refactoring

--- head.js
+++ head.js
@@ -1,15 +1,11 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: , ${actual} , ' === ' , ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: , ${actual} , ' !== ' , ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(Array, element){
 return Array[0];
 return element;
 }
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+//assertEqual(head([5,6,7]), 5);
+//assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+module.exports = head;
