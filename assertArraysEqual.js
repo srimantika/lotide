46bd3a9b8ce903325318100181324272438497b2
srimantika <srimantika@gmail.com>
2021-11-01 18:01:28 +0000

    AssertArraysEqual Refactoring

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,13 +1,12 @@
 
+const eqArrays = require("./eqArrays");
 
-const assertArraysEqual = function(Array1, Array2){
-  if (JSON.stringify(Array1) === JSON.stringify(Array2)) {
-    console.log(`✅✅✅ Assertion Passed:  ${Array1}  ' === '  ${Array2}`);
-   }
-   else {
-    console.log(`🛑🛑🛑 Assertion Failed:  ${Array1}  ' !== '  ${Array2}`);
-   }
-}
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅  Assertion passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${array1} !== ${array2}`);
+  }
+};
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 3]);
+module.exports = assertArraysEqual;
