cd3c280a3ca1f918749ac671ee51bd10914392dc
srimantika <srimantika@gmail.com>
2021-11-01 17:42:20 +0000

    Refactoring

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: , ${actual} , ' === ' , ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: , ${actual} , ' !== ' , ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(Array1, Array2){
   if (JSON.stringify(Array1) === JSON.stringify(Array2)) {
@@ -15,6 +9,8 @@ const eqArrays = function(Array1, Array2){
    }
 }
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 3]), false);
+module.exports = eqArrays;
+
+//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+//assertEqual(eqArrays([1, 2, 3], [3, 2, 3]), true);
+//assertEqual(eqArrays([1, 2, 3], [3, 2, 3]), false);
