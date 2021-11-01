4d958c8836324f94255b59063e5e024b03eea12d
srimantika <srimantika@gmail.com>
2021-11-01 18:13:35 +0000

    Refactoring middle

--- middle.js
+++ middle.js
@@ -1,20 +1,3 @@
-const assertArraysEqual = function(Array1, Array2) {
-  if (JSON.stringify(Array1) === JSON.stringify(Array2)) {
-    console.log(`✅✅✅ Assertion Passed:  ${Array1}  ' === '  ${Array2}`);
-   }
-   else {
-    console.log(`🛑🛑🛑 Assertion Failed:  ${Array1}  ' !== '  ${Array2}`);
-   }
-};
-
-const eqArrays = function(Array1, Array2) {
-  if (JSON.stringify(Array1) === JSON.stringify(Array2)) {
-    return true;
-   }
-   else {
-     return false;
-   }
-};
 
 const middle = function(Array) {
 let middleArray = []; 
@@ -30,10 +13,7 @@ middleArray = [Array[Math.floor(Array.length / 2)]];
 return middleArray;
 };
 
-console.log(middle([1, 2]));
-console.log(middle([1]));
-console.log(middle([1, 2, 3, 4, 5]));
-console.log(middle([1, 2, 3, 4, 5, 6]));
 
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3,4]));
-console.log(assertArraysEqual(middle([1, 2,3,4,5]), [3]));
+
+
+module.exports = middle;
